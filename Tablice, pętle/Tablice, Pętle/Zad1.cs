64b60aa6196230d16ef40831c73561025ca1cebd
Karolina Lewińska <lewinskak95@gmail.com>
2021-03-10 15:00:22 +0100

    Add files via upload

---
+++ Tablice, pętle/Tablice, Pętle/Zad1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba = 100;
+            int licznik = 100;
+
+            for (int i = 100; i >= 1; i--)
+            {
+                if (i % 3 == 0 && !(i % 2 == 0))
+                {
+                    liczba = i;
+                    Console.WriteLine(liczba);
+                }
+            }
+
+            while (licznik >= 1)
+            {
+                licznik--;
+                if (licznik % 3 == 0 && !(licznik % 2 == 0))
+                {
+                    liczba = licznik;
+                    Console.WriteLine(liczba);
+                }
+            }
+
+            do
+            {
+                if (licznik % 3 == 0 && !(licznik % 2 == 0))
+                {
+                    liczba = licznik;
+                    Console.WriteLine(liczba);
+                }
+                licznik--;
+            } while (licznik >= 1);
+        }
+    }
+}
---
+++ Tablice, pętle/Tablice, Pętle/Zad2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] tab = new int[4][];
+            int licznik = 1;
+            int licznik2 = 10;
+
+            for (int i = 0; i < 4; i++)
+            {
+                tab[i] = new int[licznik];
+                licznik = licznik + 1;
+            }
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                for (int j = 0; j < tab[i].Length; j++)
+                {
+                    tab[i][j] = licznik2--;
+                }
+            }
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                for (int j = 0; j < tab[i].Length; j++)
+                {
+                    Console.Write(tab[i][j] + "\n");
+                }
+            }
+        }
+    }
+}
