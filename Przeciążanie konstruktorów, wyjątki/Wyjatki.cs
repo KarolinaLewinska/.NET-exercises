707dda8c7800b6cbeed6b6fc4df291a4bc958429
Karolina Lewińska <lewinskak95@gmail.com>
2021-03-16 09:59:20 +0100

    Add files via upload

---
+++ Przeciążanie konstruktorów, wyjątki/Wyjatki.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Wyjątki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    int x = 20;
+                    int y = 0;
+                    int z = x / y;
+                }
+                catch (ArithmeticException e)
+                {
+                    throw new ArithmeticException("Dzielenie przez zero niedozwolone!", e);
+                }
+            }
+            catch(Exception exc)
+            {
+                throw new Exception("Wyjątek systemowy!", exc);
+            }
+        }
+    }
+}
