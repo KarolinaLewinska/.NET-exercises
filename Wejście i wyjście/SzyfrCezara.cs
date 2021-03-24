453b48571158fc1ed716ecdc27276762e67ca002
Karolina Lewińska <lewinskak95@gmail.com>
2021-03-24 13:23:07 +0100

    Add files via upload

---
+++ Wejście i wyjście/SzyfrCezara.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SzyfrCezara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz małe litery do zaszyfrowania");
+            ConsoleKeyInfo keyInfo;
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+                if (keyInfo.KeyChar >= 97 && keyInfo.KeyChar <= 122)
+                {
+                    char znak = (char)(((keyInfo.KeyChar - 96) % 27) + 97);
+                    Console.Write(znak);
+                }
+            } while (keyInfo.Key != ConsoleKey.Escape);
+        }
+    }
+}
